e81e52d16610976a6e67bfcee627a27af00bcb0f
shubham landge <l.shubham9237@gmail.com>
2021-08-19 14:34:24 +0530

    Minor changes

--- buisnesscard/src/app/shared/shared-module.ts
+++ buisnesscard/src/app/shared/shared-module.ts
@@ -3,14 +3,18 @@ import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
   declarations: [HeaderComponent,FooterComponent],
   imports: [
     CommonModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatButtonModule,
   ],
-  exports:[HeaderComponent,FooterComponent]
+  exports:[HeaderComponent,FooterComponent,MatToolbarModule,
+    MatButtonModule,
+]
 })
 
 export class SharedModule { }
