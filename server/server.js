a968e2209ec4a6e16d464fbb9035b8ed9947db7e
Dev-Akshay-Shelke <dev.akshayshelke@gmail.com>
2021-08-21 15:46:28 +0530

    basic server to angular connection setup

--- server/server.js
+++ server/server.js
@@ -1,25 +1,15 @@
-const express = require('express');
-const path = require('path');
-const http = require('http');
-const mongoose = require('mongoose');
-// const routes = require('./server/routes');
-const app = express();
+var express = require('express');
+var path = require('path');
+var cors = require('cors');
+
+
+var app = express();
+
+
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, '../buisnesscard/dist/buisnesscard')));
+app.use(cors());
 
-
-app.use(express.static(path.join(__dirname, 'dist')));
-// app.use('/api', routes);
-app.get('*', function (req, res) {
-  res.sendFile(path.join(__dirname, './businesscard/dist/index.html'));
-});
-// mongoose.connect('mongodb://127.0.0.1:27017/mean_tutorial');
-// mongoose.connection.on('error', console.error.bind(console, 'Database connection error:'));
-// mongoose.connection.once('open', function () {
-//   console.info('Successfully connected to the database');
-// });
-const port = process.env.PORT || '3000';
-app.set('port', port);
-const server = http.createServer(app);
-server.listen(port, function () {
-  console.info(`Server started on http://localhost:${port}`)
-});
+app.listen(3000)
+console.log("🚀 App listening on Server 3000")
