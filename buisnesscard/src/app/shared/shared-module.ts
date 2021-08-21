267faa90ce8b242bb024300405e15ec01217595d
shubham landge <l.shubham9237@gmail.com>
2021-08-21 12:52:38 +0530

    added font awesome and changes in footer links

--- buisnesscard/src/app/shared/shared-module.ts
+++ buisnesscard/src/app/shared/shared-module.ts
@@ -2,23 +2,30 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatButtonModule} from '@angular/material/button';
-import {MatCardModule} from '@angular/material/card';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatInputModule} from '@angular/material/input';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
-  declarations: [HeaderComponent,FooterComponent],
+  declarations: [HeaderComponent, FooterComponent],
   imports: [
     CommonModule,
     MatToolbarModule,
     MatButtonModule,
     MatCardModule,
     MatFormFieldModule,
-    MatInputModule
+    MatInputModule,
   ],
-  exports:[HeaderComponent,MatInputModule,FooterComponent,MatToolbarModule,MatButtonModule,MatCardModule,MatFormFieldModule]
+  exports: [
+    HeaderComponent,
+    MatInputModule,
+    FooterComponent,
+    MatToolbarModule,
+    MatButtonModule,
+    MatCardModule,
+    MatFormFieldModule,
+  ],
 })
-
-export class SharedModule { }
+export class SharedModule {}
