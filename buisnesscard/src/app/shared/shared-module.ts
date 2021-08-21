5aecda0d6789a93ec657b0284f1aaf8db8953bc0
shubham landge <l.shubham9237@gmail.com>
2021-08-21 12:32:59 +0530

    minor changes

--- buisnesscard/src/app/shared/shared-module.ts
+++ buisnesscard/src/app/shared/shared-module.ts
@@ -2,23 +2,39 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatButtonModule} from '@angular/material/button';
-import {MatCardModule} from '@angular/material/card';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatInputModule} from '@angular/material/input';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
-  declarations: [HeaderComponent,FooterComponent],
+  declarations: [HeaderComponent, FooterComponent],
   imports: [
     CommonModule,
     MatToolbarModule,
     MatButtonModule,
     MatCardModule,
     MatFormFieldModule,
-    MatInputModule
+    MatInputModule,
+    MatIconModule,
+    FlexLayoutModule,
+    FontAwesomeModule
   ],
-  exports:[HeaderComponent,MatInputModule,FooterComponent,MatToolbarModule,MatButtonModule,MatCardModule,MatFormFieldModule]
+  exports: [
+    HeaderComponent,
+    MatIconModule,
+    MatInputModule,
+    FlexLayoutModule,
+    FooterComponent,
+    MatToolbarModule,
+    MatButtonModule,
+    MatCardModule,
+    MatFormFieldModule,
+    FontAwesomeModule
+  ],
 })
-
-export class SharedModule { }
+export class SharedModule {}
