d6a5b01a2dd19bfebacc534e97343cb6ef9b5175
Dev-Akshay-Shelke <dev.akshayshelke@gmail.com>
2021-08-23 21:24:25 +0530

    new design layout updated

--- buisnesscard/src/app/home/body/body.component.ts
+++ buisnesscard/src/app/home/body/body.component.ts
@@ -1,15 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import $ from 'jquery';
 
 @Component({
   selector: 'app-body',
   templateUrl: './body.component.html',
-  styleUrls: ['./body.component.scss']
+  styleUrls: ['./body.component.scss'],
 })
 export class BodyComponent implements OnInit {
+  selectedTab = 'Connect';
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  ngAfterViewInit(): void {
+    let tabs = $('.tabs');
+    let selector = $('.tabs').find('a').length;
+    let activeItem = tabs.find('.active');
+    let activeWidth = activeItem.innerWidth();
+    $('.selector').css({
+      left: activeItem.position.left + 'px',
+      width: activeWidth + 'px',
+    });
+
+    $('.tabs').on('click', 'a', function (e) {
+      e.preventDefault();
+      $('.tabs a').removeClass('active');
+      $(this).addClass('active');
+      let activeWidth = $(this).innerWidth();
+      let itemPos = $(this).position();
+      $('.selector').css({
+        left: itemPos.left + 'px',
+        width: activeWidth + 'px',
+      });
+    });
   }
-
 }
--- server/server.js
+++ server/server.js
@@ -1,24 +1,22 @@
-
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 const path = require("path");
-const mongoose = require("mongoose")
-const db = require("./db/mongodb")
-const header_middleware = require("./middlewares/header")
+const mongoose = require("mongoose");
+const db = require("./db/mongodb");
+const header_middleware = require("./middlewares/header");
 const userRoutes = require("./Routes/userRoute");
-
 
 app.use(express.json());
 app.use(header_middleware);
 app.use("/api/user", userRoutes);
 
-app.use(express.static(process.cwd()+"./../buisnesscard/dist/buisnesscard/"));
+app.use(express.static(process.cwd() + "/buisnesscard"));
 
-app.get('/*', (req,res) => {
-  res.sendFile(path.resolve(process.cwd()+"./../buisnesscard/dist/buisnesscard/index.html"))
+app.get("/*", (req, res) => {
+  res.sendFile(path.resolve(process.cwd() + "/buisnesscard/index.html"));
 });
 
 app.listen(port, () => {
-    console.log(`Server listening on the port::${port}`);
+  console.log(`Server listening on the port::${port}`);
 });
