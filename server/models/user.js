41550e53cd4b6da17b37cef921dbfa6ad00e65d6
Dev-Akshay-Shelke <dev.akshayshelke@gmail.com>
2021-08-26 09:53:01 +0530

    User Schema Created

--- server/models/user.js
+++ server/models/user.js
@@ -1,23 +1,44 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const Profile = mongoose.model('Profile', {
-    username: {
-        type: String,
-        required: true,
-        unique: true
-    },
-
-    bio: {
-        type: String,
-        required: true
-    },
-    imagePath: { 
-        type: String,
-         required: true 
-        },
-
-    creator: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true }
+const Profile = mongoose.model("Profile", {
+  firstname: {
+    type: String,
+    required: true,
+  },
+  lastname: {
+    type: String,
+    required: true,
+    // unique: true,
+  },
+  profession: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  description: {
+    type: String,
+  },
+  contactNo: {
+    type: Number,
+  },
+  email: {
+    type: String,
+    trim: true,
+    lowercase: true,
+    unique: true,
+    required: "Email address is required",
+    validate: [validateEmail, "Please fill a valid email address"],
+    match: [
+      /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+      "Please fill a valid email address",
+    ],
+  },
+  websitelink: {
+    type: String,
+  },
+  socialLink: [],
+  backgroundUrl: { type: String },
+  profilrImageUrl: { type: String },
 });
 
-
-module.exports = Profile
+module.exports = Profile;
